cc1fb82377efdc37a81d7df383348133e9d0da91
omar56663313 <nicolas.osobustos@gmail.com>
2026-03-22 20:37:42 -0500

    Template hecho

---
+++ src/main/java/com/uq/analisis/AnalisisApplication.java
@@ -0,0 +1,13 @@
+package com.uq.analisis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalisisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalisisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uq/analisis/AnalisisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uq.analisis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalisisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
